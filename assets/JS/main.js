d105598ba4e465683f2eef3fb156cce23166d7c9
gehrig rankin <rgrankin22@gmail.com>
2018-04-24 13:54:35 -0700

    fixes

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,105 +1,113 @@
-/* global firebase moment */
-// Steps to complete:
-
-// 1. Initialize Firebase
-// 2. Create button for adding new employees - then update the html + update the database
-// 3. Create a way to retrieve employees from the employee database.
-// 4. Create a way to calculate the months worked. Using difference between start and current time.
-//    Then use moment.js formatting to set difference in months.
-// 5. Calculate Total billed
-
-// 1. Initialize Firebase
+// Initialize Firebase
 var config = {
-    apiKey: "AIzaSyA_QypGPkcjPtylRDscf7-HQl8ribnFeIs",
-    authDomain: "time-sheet-55009.firebaseapp.com",
-    databaseURL: "https://time-sheet-55009.firebaseio.com",
-    storageBucket: "time-sheet-55009.appspot.com"
+    apiKey: "AIzaSyAMw103wYrXOt0ZUGIg8mcJSuRam0eSbA0",
+    authDomain: "train-time-tracker-94cfa.firebaseapp.com",
+    databaseURL: "https://train-time-tracker-94cfa.firebaseio.com",
+    projectId: "train-time-tracker-94cfa",
+    storageBucket: "",
+    messagingSenderId: "793875369775"
 };
-  
+
 firebase.initializeApp(config);
-  
+
 var database = firebase.database();
 
-// 2. Button for adding Employees
-$("#add-train-btn").on("click", function(event) {
+
+//Button for adding trains
+$('#add-train-btn').on('click', function(){
+    //Prevents blanks?
     event.preventDefault();
 
-    // Grabs user input
-    var empName = $("#train-name-input").val().trim();
-    var empRole = $("#destination-input").val().trim();
-    var empStart = moment($("#first-start-input").val().trim(),"DD/MM/YY").format("X");
-    var empRate = $("#freq-input").val().trim();
+    //Grabs the user input
+    var trainName = $("#name-input").val().trim();
+    var trainDest = $("#dest-input").val().trim();
+    var trainStart = moment($("#start-input").val().trim(),"HH:mm").format("X");
+    var trainFreq = $("#freq-input").val().trim();
 
-    // Creates local "temporary" object for holding employee data
-    var newEmp = {
-        name: empName,
-        role: empRole,
-        start: empStart,
-        rate: empRate
+    // Creates local "temporary" object for holding train data
+    var newTrain = {
+        name: trainName,
+        dest: trainDest,
+        start: trainStart,
+        freq: trainFreq
     };
 
-    // Uploads employee data to the database
-    database.ref().push(newEmp);
+    // Uploads train data to the database
+    database.ref().push(newTrain);
 
-    // Logs everything to console
-    console.log(newEmp.name);
-    console.log(newEmp.role);
-    console.log(newEmp.start);
-    console.log(newEmp.rate);
+    console.log(newTrain.name);
+    console.log(newTrain.dest);
+    console.log(newTrain.start);
+    console.log(newTrain.freq);
 
-    // Alert
-    alert("Employee successfully added");
+
+    alert("Train successfully added");
 
     // Clears all of the text-boxes
-    $("#train-name-input").val("");
-    $("#destination-input").val("");
-    $("#first-start-input").val("");
+    $("#name-input").val("");
+    $("#dest-input").val("");
+    $("#start-input").val("");
     $("#freq-input").val("");
 });
 
-// 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
-database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+// Create Firebase event for adding train to the database and a row in the html when a user adds an entry
+
+database.ref().on('child_added', function(childSnapshot, prevChildKey){
+
     console.log(childSnapshot.val());
 
     // Store everything into a variable.
-    var empName = childSnapshot.val().name;
-    var empRole = childSnapshot.val().role;
-    var empStart = childSnapshot.val().start;
-    var empRate = childSnapshot.val().rate;
+    var trainName = childSnapshot.val().name;
+    var trainDest = childSnapshot.val().dest;
+    var trainStart = childSnapshot.val().start;
+    var trainFreq = childSnapshot.val().freq;
 
-    // Employee Info
-    console.log(empName);
-    console.log(empRole);
-    console.log(empStart);
-    console.log(empRate);
+    // Train Info
+    console.log(trainName);
+    console.log(trainDest);
+    console.log(trainStart);
+    console.log(trainFreq);
 
-    // Prettify the employee start
-    var empStartPretty = moment.unix(empStart).format("MM/DD/YY");
+
+    //Calculating the next train
+    
+    
 
     // Calculate the months worked using hardcore math
     // To calculate the months worked
-    var empMonths = moment().diff(moment.unix(empStart, "X"), "months");
-    console.log(empMonths);
+    //var train = moment().diff(moment.unix(empStart, "X"), "months");
+    //console.log(empMonths);
 
     // Calculate the total billed rate
-    var empBilled = empMonths * empRate;
-    console.log(empBilled);
+    //var empBilled = empMonths * empRate;
+    //console.log(empBilled);
 
+    var nextTrainPretty = moment.unix(trainStart).format("HH:mm");
+
+    var minAway = moment().diff(moment.unix(trainStart, "X"), "minutes");
+
+    var now = new Date();
+    var h = now.getHours();
+    var m = now.getMinutes();
+
+    
+    if ((m + trainFreq) > 60){
+        h++;
+        m = 60 - (m + trainFreq);
+    }
+    
+    
+
+
+    
     // Add each train's data into the table
     $("#train-table > tbody").append(
-        "<tr><td>" + empName + 
-        "</td><td>" + empRole + 
-        "</td><td>" + empStartPretty +
-        "</td><td>" + empMonths +
-        "</td><td>" + empRate +
+        "<tr><td>" + trainName + 
+        "</td><td>" + trainDest + 
+        "</td><td>" + trainFreq +
+        "</td><td>" + nextTrainPretty +
+        "</td><td>" + minAway +
         "</td><td>"
     );
 });
-
-// Example Time Math
-// -----------------------------------------------------------------------------
-// Assume Employee start date of January 1, 2015
-// Assume current date is March 1, 2016
-
-// We know that this is 15 months.
-// Now we will create code in moment.js to confirm that any attempt we use meets this test case
